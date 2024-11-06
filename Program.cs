5c692b63025df0550ce509a13ca60313fbc104cb
Birka <birkavidit@gmail.com>
2024-11-06 14:17:52 +0100

    first iteration

--- Program.cs
+++ Program.cs
@@ -2,7 +2,6 @@ using StereoKit;
 using System;
 using System.Net;
 using System.Net.Sockets;
-using System.Threading.Tasks;
 
 namespace Coop_Vr
 {
@@ -19,30 +18,12 @@ namespace Coop_Vr
             if (!SK.Initialize(settings))
                 Environment.Exit(1);
 
+            
+            Action stepper = new Server().Step;
+            //Action stepper = new Client().Step;
 
-            // Create assets used by the app
-            Pose cubePose = new Pose(0, 0, -0.5f, Quat.Identity);
-            Model cube = Model.FromMesh(
-                Mesh.GenerateRoundedCube(Vec3.One * 0.1f, 0.02f),
-                Default.MaterialUI);
-
-            Matrix floorTransform = Matrix.TS(0, -1.5f, 0, new Vec3(30, 0.1f, 30));
-            Material floorMaterial = new Material(Shader.FromFile("floor.hlsl"));
-            floorMaterial.Transparency = Transparency.Blend;
-
-            Action stepper = Client();
-            // Core application loop
             while (SK.Step(() =>
             {
-                if (SK.System.displayType == Display.Opaque)
-                    Default.MeshCube.Draw(floorMaterial, floorTransform);
-
-                UI.Handle("Cube", ref cubePose, cube.Bounds);
-                cube.Draw(cubePose.ToMatrix());
-
-                Pose inputPos = Input.Hand(Handed.Right)[FingerId.Index, JointId.Tip].Pose;
-                Text.Add(inputPos.position.ToString(), inputPos.ToMatrix());
-
                 stepper?.Invoke();
             })) ;
             SK.Shutdown();
@@ -50,10 +31,20 @@ namespace Coop_Vr
 
         static Action Client()
         {
+            //hard code a port that is available
+            //var udpServer = bew UdpClient(port);
+            //.enable broadcast
+            //send message about host existing, 
+            //send tcp host port over by finding an available code (withing message of server discovery)
+            //build tcp connection between them
+            //server is tcpListener, client is tcpClient
+            //send data between them
+
+            //other clients must know the port you chose
             //info about local host
             IPHostEntry ipEntry = Dns.GetHostEntry(Dns.GetHostName());
             //local host ip address
-            IPAddress ip = ipEntry.AddressList[0];//
+            IPAddress ip = ipEntry.AddressList[0];
 
             IPEndPoint endPoint = new(ip, 1234);
 
